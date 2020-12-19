586568248b000169bb1b63a2b4e92b1fd09ef1f4
Kasun Vithanage <alankasun@gmail.com>
2020-12-20 02:33:12 +0530

    no purge

--- gridsome.config.js
+++ gridsome.config.js
@@ -122,20 +122,20 @@ module.exports = {
         height: 900,
       },
     },
-    {
-      use: "gridsome-plugin-purgecss",
-      // default options, the following will be included if you don't provide anything
-      options: {
-        content: [
-          "./src/**/*.vue",
-          "./src/**/*.js",
-          "./src/**/*.jsx",
-          "./src/**/*.pug",
-          "./src/**/*.md",
-        ],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      },
-    },
+    // {
+    //   use: "gridsome-plugin-purgecss",
+    //   // default options, the following will be included if you don't provide anything
+    //   options: {
+    //     content: [
+    //       "./src/**/*.vue",
+    //       "./src/**/*.js",
+    //       "./src/**/*.jsx",
+    //       "./src/**/*.pug",
+    //       "./src/**/*.md",
+    //     ],
+    //     defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    //   },
+    // },
   ],
 
   transformers: {
